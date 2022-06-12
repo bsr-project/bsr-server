c7114a81306bb50669171201a98cb40b1f78557a
imba97 <mail@imba97.cn>
2022-06-12 18:46:51 +0800

    perf(join-mission): 添加字段 join_user_id

--- src/join-mission/join-mission.service.ts
+++ src/join-mission/join-mission.service.ts
@@ -58,6 +58,7 @@ export class JoinMissionService {
       mission_id: createJoinMissionDto.mission_id,
       submission_id: _.join(createJoinMissionDto.submission_id, ','),
       user_id: user.id,
+      join_user_id: user.id,
       sign_in_time: createJoinMissionDto.datetime,
       sign_in_vehicle: createJoinMissionDto.vehicle,
       sign_in_custom_vehicle: createJoinMissionDto.custom_vehicle,
